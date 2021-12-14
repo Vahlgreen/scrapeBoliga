f1b2824cc19f1d9d6dab9c7e2b3942962569e1a2
Vahlgreen <vahlgreen@live.dk>
2021-12-14 10:22:05 +0100

    boliga scraper now works

--- boliga/boliga/spiders/boliga_spider.py
+++ boliga/boliga/spiders/boliga_spider.py
@@ -1,29 +1,41 @@
+
 import scrapy
-import time
 
 class BoligaSpider(scrapy.Spider):
     name = "boliga"
+    start_urls = []
+    for i in range(1,261):
+        start_urls.append(f"https://www.boliga.dk/salg/resultater?sort=omregnings_dato-d&kode=3&fraPostnr=&tilPostnr=&minsaledate=2004&maxsaledate=today&kom=&type=Villa&gade=&searchTab=1&page={i}")
 
-    #start_requests returns (via yield) a request object
-    def start_requests(self):
-        urls = [
-            'https://www.boliga.dk/salg/resultater?sort=omregnings_dato-d&kode=3&fraPostnr=&tilPostnr=&minsaledate=2004&maxsaledate=today&kom=461&type=Villa&gade=&searchTab=1&page=1'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
 
-    #when start_request is called, scrapy.request returns a response object that is parsed to parse
+
+
+    #['https://www.boliga.dk/salg/resultater?sort=omregnings_dato-d&kode=3&fraPostnr=&tilPostnr=&minsaledate=2004&maxsaledate=today&kom=&type=Villa&gade=&searchTab=1&page=1']
+    #,"https://www.boliga.dk/salg/resultater?sort=omregnings_dato-d&kode=3&fraPostnr=&tilPostnr=&minsaledate=2004&maxsaledate=today&kom=461&type=Villa&gade=&searchTab=1&page=2",
+    #   "https://www.boliga.dk/salg/resultater?sort=omregnings_dato-d&kode=3&fraPostnr=&tilPostnr=&minsaledate=2004&maxsaledate=today&kom=461&type=Villa&gade=&searchTab=1&page=3"]
+
+
     def parse(self, response):
 
         filename = 'boliga.txt'
-        with open(filename, 'w') as f:
+        with open(filename, 'a') as f:
 
-            for i in range(1,50):
-                row = response.css(f"table > tbody > tr:nth-child({i}) > td:nth-child(2) > span::text").get()
-                if row is not None:
-                    row = row[:-4]
-                f.write(f"{row}\n")
+            for i in range(1, 50):
+                #houseprice
+                price = response.css(
+                    f"table > tbody > tr:nth-child({i}) > td:nth-child(2) > span::text").get()
+                if price is not None:
+                    price = price[:-4]
 
+
+                #sold date
+                sold_date=response.css(f"table > tbody > tr:nth-child({i}) > td:nth-child(3) > div > span:nth-child(1)::text").get()
+                sqm = response.css(f"table > tbody > tr:nth-child({i}) > td:nth-child(4) > div > span:nth-child(1)::text").get()
+                price_sqm = response.css(f"table > tbody > tr:nth-child({i}) > td:nth-child(4) > div > span.text-nowrap.mt-1::text").get()
+                build_year = response.css(f"table > tbody > tr:nth-child({i}) > td:nth-child(6) > span::text").get()
+                vejnavnnr = response.css(f"table > tbody > tr:nth-child({i}) > td.table-col.d-flex.align-items-center.address-cell > a::text").get()
+                kommune = response.xpath(f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[1]/a/text()[2]").get()
+
+
+                f.write(f"{price};{sold_date};{sqm};{price_sqm};{build_year};{vejnavnnr};{kommune}\n")
         self.log(f'Saved file {filename}')
-
-
