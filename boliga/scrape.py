c824eb67991a66d66e369e043230eeb8c6d78c5b
Vahlgreen <vahlgreen@live.dk>
2022-01-18 21:54:00 +0100

    must speed up process and wait for ui elements

--- boliga/scrape.py
+++ boliga/scrape.py
@@ -3,47 +3,57 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
+import re
+import os
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get("https://www.boliga.dk/salg/resultater?searchTab=1&page=1&sort=date-d&salesDateMin=2004&salesDateMax=2022&saleType=1")
-time.sleep(5)
+time.sleep(3)
 driver.find_element(By.XPATH,"//button[@id='declineButton']").click()
-#time.sleep(2)
 next_button_xpath = "/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/div/div/app-pagination/div/div[4]/a"
-#driver.find_element(By.XPATH,next_button_xpath).click()
 time.sleep(0.5)
 #print(driver.page_source)
 #print(driver.current_url)
 
+
+if os.path.exists("alle_salg.csv"):
+    os.remove("alle_salg.csv")
+
+num_pages = 50
+
+
 with open("alle_salg.csv",'a') as f:
-    for j in range(1,5):
-        for i in range(1,50):
+    for j in range(1,num_pages):
+        time_start = time.time()
+        for i in range(1,51):
 
             #pris
-            print(driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[2]/span").text)
             price = driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[2]/span").text
+            price = price[:-4]#remove units
+
             #adresse
-            print(driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[1]/a").text)
             adress = driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[1]/a").text
+            adress = adress.replace("\n"," ").replace(",","")
+            zip_code = re.search("\d{4}.*",adress).group()
+            street = adress.replace(f" {zip_code}","")
+
             #dato
-            print(driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[3]/div/span[1]").text)
             sold_date = driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[3]/div/span[1]").text
             #byggår
-            print(driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[6]/span").text)
             build_year = driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[6]/span").text
             #kvm
-            print(driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[4]/div/span[1]").text)
             sqm = driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[4]/div/span[1]").text
+            sqm = sqm[:-2]
             #kvm pris
-            print(driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[4]/div/span[2]").text)
             price_sqm = driver.find_element(By.XPATH,f"/html/body/app-root/app-scroll-position-restoration/app-main-layout/app-sold-properties-list/div[3]/app-sold-list-table/table/tbody/tr[{i}]/td[4]/div/span[2]").text
-            print("")
-            f.write(f"{price},{sold_date},{sqm},{price_sqm},{build_year},{adress}\n")
+            price_sqm = price_sqm[:-6]
+
+            f.write(f"{street},{zip_code},{price},{sold_date},{sqm},{price_sqm},{build_year}\n")
+        time_end = time.time()
+        print(f"Time passed: {(time_end - time_start)}")
         driver.find_element(By.XPATH, next_button_xpath).click()
-        time.sleep(0.5)
-while True:
-    pass
+        time.sleep(0.4)
+
 
 #from selenium import webdriver
 #browser exposes an executable file
